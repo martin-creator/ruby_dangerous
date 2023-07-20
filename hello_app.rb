f1ec14c3beef7813ce5888b949e380504773462e
martin-creator <martinlubowa@outlook.com>
2023-07-20 23:59:13 +0300

    Added more endpoints to sinatra app

--- hello_app.rb
+++ hello_app.rb
@@ -1,5 +1,27 @@
 require 'sinatra'
 
+
+# This is a Sinatra application
 get '/' do
     'Hello world!'
+end
+
+get '/about' do
+    'A little about me.'
+end
+
+get '/hello/:name' do
+    "Hello there, #{params[:name].upcase}."
+end
+
+get '/hello/:name/:city' do
+    "Hey there #{params[:name]} from #{params[:city]}."
+end
+
+get '/more/*' do
+    params[:splat]
+end
+
+get '/form' do
+    erb :form
 end
