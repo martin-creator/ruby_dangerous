8743fb113e5cd72aff9310baf3e1ae41cedf4d61
martin-creator <martinlubowa@outlook.com>
2023-07-24 15:43:48 +0300

    Added some math methods

---
+++ native_objects.rb
@@ -0,0 +1,115 @@
+# Ruby Math Methods
+
+# https://ruby-doc.org/core-2.5.1/Math.html
+
+
+# Example one
+p Math.sqrt(9) # => 3.0
+
+# Example two
+p Math.log(2) # => 0.6931471805599453
+
+# Example three
+p Math.log10(10) # => 1.0
+
+# Example four
+p Math.log2(2) # => 1.0
+
+# Example five
+p Math.sin(90) # => 0.8939966636005579
+
+# Example six
+p Math.cos(90) # => -0.4480736161291701
+
+# Example seven
+p Math.tan(90) # => -1.995200412208242
+
+# Example eight
+p Math.hypot(3, 4) # => 5.0
+
+# Example nine
+p Math.acos(0.5) # => 1.0471975511965979
+
+# Example ten
+p Math.asin(0.5) # => 0.5235987755982989
+
+# Example eleven
+p Math.atan(0.5) # => 0.4636476090008061
+
+# Example twelve
+p Math.atan2(0.5, 0.5) # => 0.7853981633974483
+
+# Example thirteen
+p Math.cosh(90) # => 6.102016471589204e+38
+
+# Example fourteen
+p Math.sinh(90) # => 6.102016471589204e+38
+
+# Example fifteen
+p Math.tanh(90) # => 1.0
+
+# Example sixteen
+p Math.acosh(90) # => 5.192925985263684
+
+# Example seventeen
+p Math.asinh(90) # => 5.192987713658941
+
+# Example eighteen
+p Math.atanh(90) # => 0.5493061443340549
+
+# Example nineteen
+p Math.erf(90) # => 1.0
+
+# Example twenty
+p Math.erfc(90) # => 0.0
+
+# Example twenty-one
+p Math.gamma(90) # => Infinity
+
+# Example twenty-two
+p Math.lgamma(90) # => Infinity
+
+# Example twenty-three
+p Math.frexp(90) # => [0.703125, 7]
+
+# Example twenty-four
+p Math.ldexp(90, 90) # => 1.2379400392853803e+273
+
+# examples of arithmetic operators
+
+# example one
+p 1 + 1 # => 2
+
+# example two
+
+p 1 - 1 # => 0
+
+# example three
+
+p 1 * 1 # => 1
+
+# example four
+
+p 1 / 1 # => 1
+
+
+# example five
+
+p 1 % 1 # => 0
+
+# example six
+
+p 2 ** 2 # => 4
+
+# example seven
+
+p 2 ** 3 # => 8
+
+# example eight
+
+p 2 ** 4 # => 16
+
+# example nine
+
+p 2 ** 5 # => 32
+
