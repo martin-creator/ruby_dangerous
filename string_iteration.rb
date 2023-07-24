a2676cba43cbbbac9184055b10c7c15dd11459dc
martin-creator <martinlubowa@outlook.com>
2023-07-24 12:57:26 +0300

    Added methods to iterate over the string

---
+++ string_iteration.rb
@@ -0,0 +1,31 @@
+# String Iteration
+
+# Path: string_iteration.rb
+# Compare this snippet from strings.rb:
+
+# # https://ruby-doc.org/3.2.2/String.html
+
+# loop through each character in a string
+
+puts "hello".each_char { |char| puts char }
+
+# loop through each character in a string
+ for i in 0..5 do
+    puts "hello"[i]
+ end
+
+# loop through each character in a string
+
+p "hello".split("").each { |char| puts char }
+
+
+# loop through each character in a string
+
+p "hello".chars.each { |char| puts char }
+
+# soliloquy = "To be, or not to be, that is the question:
+
+
+
+
+
