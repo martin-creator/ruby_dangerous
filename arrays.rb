2f4d6c7603df4313e6fd24d2ec6246bb2df43734
martin-creator <martinlubowa@outlook.com>
2023-07-24 13:00:26 +0300

    Used string split

---
+++ arrays.rb
@@ -0,0 +1,21 @@
+# Ruby Arrays
+
+# convert string to array
+
+ p "hello".split("") # => ["h", "e", "l", "l", "o"]
+
+# split on a specific character
+
+    p "hello world".split(" ") # => ["hello", "world"]
+
+# convert array to string
+
+    p ["a", "b", "c"].join # => "abc"
+
+# convert array to string
+
+    p ["a", "b", "c"].join("-") # => "a-b-c"
+
+# convert array to string
+
+    p ["a", "b", "c"].join(", ") # => "a, b, c"
