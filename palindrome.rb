af616a213d3004980cee43e66cc75a4af1eb5544
martin-creator <martinlubowa@outlook.com>
2023-08-07 15:32:55 +0300

    Added info about blocks

--- palindrome.rb
+++ palindrome.rb
@@ -6,3 +6,26 @@ def palindrome?(string)
   processed_content = string.downcase
   processed_content == processed_content.reverse
 end
+
+# blocks in ruby
+
+(1..10).each do |number| 
+    puts "#{number} situp"
+end
+
+# yield keyword is used to call the block
+
+def n_times(number)
+    1.upto(number) do |count|
+        yield count
+    end
+end
+
+n_times(10) do |n|
+    puts "#{n} situp"
+    puts "#{n} pushup"
+    puts "#{n} chinup"
+end
+
+
+    
