0b63c26d9eb63c1b7f2b446d188a1ebef8108f0c
martin-creator <martinlubowa@outlook.com>
2023-08-11 19:51:58 +0300

    Added more class examples

--- object_oriented.rb
+++ object_oriented.rb
@@ -26,3 +26,50 @@ puts phrase.content
 # phrase.content = "This is a new phrase" # this is a setter method
 # puts phrase.content
 puts phrase.palindrome?
+
+
+# Example 2 using class
+class Phrase
+    def initialize(content)
+        @content = content
+    end
+
+    # returns true if the phrase is a palindrome, false otherwise
+    def palindrome?
+        processed_content == processed_content.reverse
+    end
+
+    private # this makes the method private and can only be used within the class
+    # private methods can be used by instance methods but not by other objects
+    # private methods cannot be called with an explicit receiver
+    # private methods are available to subclasses that inherit from the class in which the private method is declared
+
+    # returns content for palindrome testing
+    def processed_content
+        @content.downcase
+    end
+end
+
+phrase = Phrase.new("racecar")
+puts phrase.palindrome?
+
+
+# Example 3 using inheritance
+class Book
+    attr_accessor :title, :author, :pages
+    def initialize(title, author, pages)
+        @title = title
+        @author
+        @pages = pages
+    end
+end
+
+book1 = Book.new("Harry Potter", "JK Rowling", 400)
+puts book1.title
+puts book1.author
+puts book1.pages
+
+book2 = Book.new("Lord of the Rings", "Tolkein", 500)
+puts book2.title
+puts book2.author
+puts book2.pages
