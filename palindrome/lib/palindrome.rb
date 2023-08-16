4b8714a13993c011b3fe1f72a7fc486ef4c7a465
martin-creator <martinlubowa@outlook.com>
2023-08-16 18:55:49 +0300

    created ruby gem palindrome

---
+++ palindrome/lib/palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "palindrome/version"
+
+module Palindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ palindrome/lib/palindrome/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Palindrome
+  VERSION = "0.1.0"
+end
